f7d67e54ee393ea0e140e08d101a6a0502696bda
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-09-27 20:28:22 -0500

    Remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-
-import store from './redux/store'
 
 import * as serviceWorker from './serviceWorker'
 import App from './App'
 import './global.sass'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 )
 
