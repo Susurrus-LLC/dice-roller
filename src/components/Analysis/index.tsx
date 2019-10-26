5cfc0fff3c7f91ba69d99a993baddc77cf98dca2
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-10-26 12:25:20 -0500

    Correctly compute probabilities

--- src/components/Analysis/index.tsx
+++ src/components/Analysis/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 interface Probability {
   val: number
-  prob: number
+  prob: string
 }
 
 const Analysis: React.FC<Props> = ({ dice }) => {
@@ -52,12 +52,12 @@ const Analysis: React.FC<Props> = ({ dice }) => {
 
   // calculate the total possible results (including identicals) of a set of dice
   const calcTotalPoss = (): number => {
-    let total = 0
+    let total = 1
 
     dice.forEach(die => {
       const { number, sides } = die
 
-      total += sides === 'f' ? number * 3 : number * sides
+      total *= sides === 'f' ? number * 3 : Math.pow(sides, number)
     })
 
     return total
@@ -99,36 +99,31 @@ const Analysis: React.FC<Props> = ({ dice }) => {
     return total
   }
 
+  // calculate the number of combinations of results that will succeed at reaching a target
   const calcSucc = (
     target: number,
-    count: number,
     attempts: number[],
-    roll: number,
+    die: number = 0,
     original?: number[]
   ): number => {
-    let successes = count
+    let successes = 0
+    // if this is the topmost call of the function, create the original array
+    // if this is a recursive call, pass the original array along
     const orig = original === undefined ? [...attempts] : original
     const newAttempts = [...attempts]
 
-    if (roll === 0 || newAttempts[roll] !== orig[roll]) {
-      console.log('checking success')
-      if (calcDieResults(newAttempts) === target) {
+    // cycle through all possible results with the current die
+    for (let i = newAttempts[die]; i > 0; i--) {
+      newAttempts[die] = i
+
+      // if examining the first die, or if the current die does not equal the value of its original, check for success
+      // this logic needed to prevent checking twice with each recursion
+      if ((die === 0 || newAttempts[die] !== orig[die]) && (calcDieResults(newAttempts) === target)) {
         successes++
       }
-    }
 
-    for (let i = newAttempts[roll]; i > 0; i--) {
-      console.log(newAttempts)
-      newAttempts[roll]--
-
-      if (newAttempts[roll] > 0) {
-        if (roll < orig.length - 1) {
-          return calcSucc(target, successes, newAttempts, roll + 1, orig)
-        } else {
-          return calcSucc(target, successes, newAttempts, roll, orig)
-        }
-      } else {
-        return successes
+      if (die < orig.length - 1) {
+        successes += calcSucc(target, newAttempts, die + 1, orig)
       }
     }
 
@@ -137,25 +132,31 @@ const Analysis: React.FC<Props> = ({ dice }) => {
 
   const diceProbs = (min: number, max: number): Probability[] => {
     const probabilities: Probability[] = []
-    const sidesArr = getSidesArr()
+    const poss = calcTotalPoss()
 
+    // for each possible result between the min and max, calculate how many ways that result can be rolled
     for (let i = min; i <= max; i++) {
-      console.log('start')
+      const succ = calcSucc(i, getSidesArr())
+
       probabilities.push({
         val: i,
-        prob: calcSucc(i, 0, sidesArr, 0)
+        prob: `${Math.round(succ / poss * 10000) / 100}%: ${succ} / ${poss}`
       })
     }
 
     return probabilities
   }
 
+  const min = calcMinMax('min')
+  const max = calcMinMax('max')
+
   return (
     <section className={styles.analysis}>
-      <p>Min: {calcMinMax('min')}</p>
-      <p>Max: {calcMinMax('max')}</p>
+      <p>Min: {min}</p>
+      <p>Max: {max}</p>
       <p>Avg: {calcAvg()}</p>
-      <p>Probabilities: to be calculated{/* diceProbs(min, max) */}: of {calcTotalPoss()} possible results</p>
+      <p>Probabilities: of {calcTotalPoss()} possible results</p>
+      <p>{JSON.stringify(diceProbs(min, max))}</p>
     </section>
   )
 }
